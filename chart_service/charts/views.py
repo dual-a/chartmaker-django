3be9a340bab24cf1b3ddde081364c8dcee45f1d3
Abdullah Ahmad <aabdullaahmad@gmail.com>
2024-03-18 11:42:46 +1300

    inital commit

---
+++ chart_service/charts/views.py
@@ -0,0 +1,19 @@
+import io
+from django.http import HttpResponse
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import matplotlib.pyplot as plt
+
+
+def generate_chart(request):
+    # Create a chart using Matplotlib
+    fig, ax = plt.subplots()
+    ax.bar([1, 2, 3], [4, 2, 7])
+
+    # Save the chart to a buffer
+    buffer = io.BytesIO()
+    canvas = FigureCanvasAgg(fig)
+    canvas.print_png(buffer)
+    plt.close(fig)
+
+    # Return the buffer as an image response
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
